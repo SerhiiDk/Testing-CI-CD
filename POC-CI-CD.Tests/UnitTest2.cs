df1a3637cfe8d910cb76fbe6f2d553ca70d69dba
HFGF <HFGF@DESKTOP-3FAJ25G>
2025-08-06 10:14:45 +0200

    feat: add test

--- POC-CI-CD.Tests/UnitTest2.cs
+++ POC-CI-CD.Tests/UnitTest2.cs
@@ -28,5 +28,18 @@ public class UnitTest2
         Assert.Equal(expected, result);
     }
 
+    [Fact]
+    public void Return_Unit_Failed1()
+    {
+        // Arrange
+        var a = 1;
+        var b = 2;
+        // Act
+        var expected = 4;
+        var result = a + b;
+        // Assert
+        Assert.Equal(expected, result);
+    }
+
 }
 
