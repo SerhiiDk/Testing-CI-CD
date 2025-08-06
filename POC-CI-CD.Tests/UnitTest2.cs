326afbc35ff88ce937e5a2e01a35627e5505b40f
HFGF <HFGF@DESKTOP-3FAJ25G>
2025-08-06 12:37:39 +0200

    Failed Expeced 3

--- POC-CI-CD.Tests/UnitTest2.cs
+++ POC-CI-CD.Tests/UnitTest2.cs
@@ -41,5 +41,18 @@ public class UnitTest2
         Assert.Equal(expected, result);
     }
 
+    [Fact]
+    public void Return_Unit_Failed2()
+    {
+        // Arrange
+        var a = 1;
+        var b = 2;
+        // Act
+        var expected = 4;
+        var result = a + b;
+        // Assert
+        Assert.Equal(expected, result);
+    }
+
 }
 
