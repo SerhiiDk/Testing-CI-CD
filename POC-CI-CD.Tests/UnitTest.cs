b2ec90b3f2e6d71625328d390cffdcbc56a1abf5
HFGF <HFGF@DESKTOP-3FAJ25G>
2025-08-06 10:27:13 +0200

    add new tes

--- POC-CI-CD.Tests/UnitTest.cs
+++ POC-CI-CD.Tests/UnitTest.cs
@@ -30,5 +30,19 @@ namespace POC_CI_CD.Tests
             // Assert
             Assert.Equal(expected, result);
         }
+
+        [Fact]
+        public void Return_Unit_Test_Success1()
+        {
+            // Arrange
+            var a = 1;
+            var b = 2;
+            // Act
+
+            var expected = 3;
+            var result = a + b;
+            // Assert
+            Assert.Equal(expected, result);
+        }
     }
 }
